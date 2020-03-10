2b7af203da78ed02f9ee791c47b62c9311eb1438
chenhao <chenhao@daisha.com.cn>
2020-03-10 11:05:06 +0800

    接口开发

--- web/app.py
+++ web/app.py
@@ -1,21 +1,24 @@
-from .utils import load_config
-from .logger  import setup_log
+from web.utils import load_config
+from web.logger  import setup_log
 from flask import Flask, request, render_template, session, redirect, url_for
-from .utils import mysql
+from web.utils import mysql
 import math
 
 config = load_config()
 _logger = setup_log(__name__)
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
 mysql = mysql(config['mysql'])
 
 # 模板
 # @app.route("/user", methods=['POST', 'GET'])
 # def user():
-#     """
-#     个人信息
-#     :return: UserInfo.html
-#     """
+
+# 获取指定参数：
+# request.form[""]
+# 获取指定参数并生成默认值：
+# request.form.get("key", type=str, default=None)
+
 #     login, userid = False, None
 #     if 'userid' not in session:
 #         return redirect(url_for('loginForm'))
@@ -35,7 +38,21 @@ mysql = mysql(config['mysql'])
 
 
 
+@app.route("/projectInfo",methods=["POST","GET"])
+def projectInfo():
+    """
+    项目信息，测试接口
+    :return:
+    """
+    return "本项目是一个农产品大数据接口，通过爬取网络相关产品数据，汇总+分析得到完整的数据模型。"
 
+@app.route("/parameterTest",methods=["POST"])
+def parameterTest():
+    """
+    传参测试接口
+    :return:
+    """
+    return str(request.form.to_dict())
 
 
 
